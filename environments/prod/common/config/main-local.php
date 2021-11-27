9eb284d54f8c9cfc54e51667cbb035b06342269c
Tomáš Ryšavý <xrysav27@vutbr.cz>
2021-11-27 14:28:05 +0100

    tutorial changes

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -4,9 +4,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=eporqep6b4b8ql12.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=jxr3mhdig4xed529',
+            'username' => 'hmsfwufmmq2wqypa',
+            'password' => 'tfm6wf9l5u0kxca3',
             'charset' => 'utf8',
         ],
         'mailer' => [
